ce5d87ee1389581e54f29ab59ebd3a1cb5c2956f
MinRK <benjaminrk@gmail.com>
2013-03-06 12:40:42 -0800

    initial commit with my default profile

---
+++ startup/00-start.py
@@ -0,0 +1,32 @@
+import sys,os,shutil,time
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import pkg_resources
+
+ip = get_ipython()
+from IPython.config.application import Application
+app = Application.initialized() and Application.instance()
+
+try:
+    import zmq
+except ImportError:
+    print ('no zmq')
+else:
+    ctx = zmq.Context.instance()
+    def create_bound_pair(type1=zmq.PAIR, type2=zmq.PAIR):
+        sa = ctx.socket(type1)
+        p = sa.bind_to_random_port('tcp://127.0.0.1')
+        sb = ctx.socket(type2)
+        sb.connect('tcp://127.0.0.1:%i' % p)
+        for s in (sa, sb):
+            if s.type == zmq.SUB:
+                s.subscribe = b''
+        return sa, sb
+
+iface = 'tcp://127.0.0.1:5555'
+
+try:
+    import numpy as np
+except:
+    pass
