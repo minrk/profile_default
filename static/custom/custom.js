6fa5631dc64f21c7042fbf5f009076f7d11b0e2d
MinRK <benjaminrk@gmail.com>
2014-02-08 21:15:04 -0800

    use new IPython.load_extensions API

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -1,12 +1,3 @@
 
-// load the gist extension
-
-require(["nbextensions/gist"], function (gist_extension) {
-    console.log('gist extension loaded');
-    gist_extension.load_extension();
-});
-require(["nbextensions/toc"], function (toc) {
-    console.log('Table of Contents extension loaded');
-    toc.load_extension();
-});
-
+// load the gist and toc extensions
+IPython.load_extensions("gist", "toc");
