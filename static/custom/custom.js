e77f324876ffa055aa04719b5cbf4bd75bc4c323
MinRK <benjaminrk@gmail.com>
2014-03-10 11:27:36 -0700

    autosave every 30s

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -1,3 +1,7 @@
-
 // load the gist and toc extensions
 IPython.load_extensions("gist", "toc");
+
+// autosave every 30 seconds;
+$([IPython.events]).on("notebook_loaded.Notebook", function () {
+    IPython.notebook.minimum_autosave_interval = 30000;
+});
