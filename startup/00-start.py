d8a7f729af86f1d95245dda4d32dcacbcbea1385
MinRK <benjaminrk@gmail.com>
2014-03-10 11:27:24 -0700

    del pkg_resources

--- startup/00-start.py
+++ startup/00-start.py
@@ -3,6 +3,7 @@ import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     import pkg_resources
+    del pkg_resources
 
 ip = get_ipython()
 from IPython.config.application import Application
