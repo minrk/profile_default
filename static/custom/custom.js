33e56a8dc22d6c636b356b5b16a1fdabb9b4e7c1
MinRK <benjaminrk@gmail.com>
2014-04-04 16:44:19 -0700

    disable autoscroll

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -4,4 +4,12 @@ IPython.load_extensions("gist", "toc");
 // autosave every 30 seconds;
 $([IPython.events]).on("notebook_loaded.Notebook", function () {
     IPython.notebook.minimum_autosave_interval = 30000;
+
+// autoscroll is my greatest shame:
+IPython.OutputArea.auto_scroll_threshold = 0;
+
+// when doing presentations, I hode the header and toolbar
+$([IPython.events]).on("app_initialized.NotebookApp", function () {
+    // $('div#header').hide();
+    // $('div#maintoolbar').hide();
 });
