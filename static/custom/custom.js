8494ed4c0254f779d2fecd097c85de8c9d8819d7
MinRK <benjaminrk@gmail.com>
2014-04-08 13:29:54 -0700

    hide cell toolbar selector
    I never use it

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -4,12 +4,16 @@ IPython.load_extensions("gist", "toc");
 // autosave every 30 seconds;
 $([IPython.events]).on("notebook_loaded.Notebook", function () {
     IPython.notebook.minimum_autosave_interval = 30000;
+});
 
 // autoscroll is my greatest shame:
 IPython.OutputArea.auto_scroll_threshold = 0;
 
+
 // when doing presentations, I hode the header and toolbar
 $([IPython.events]).on("app_initialized.NotebookApp", function () {
+    // hide cell toolbar select, I never use it
+    $('#ctb_select').hide().prev().hide();
     // $('div#header').hide();
     // $('div#maintoolbar').hide();
 });
