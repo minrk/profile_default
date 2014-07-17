d16cec0d6153ed75fdd78250592dcbcf604d1b81
MinRK <benjaminrk@gmail.com>
2014-07-17 11:08:17 -0700

    require-ify custom.js

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -1,23 +1,37 @@
-// load the gist and toc extensions
-IPython.load_extensions("gist", "toc");
-
-// autosave every 30 seconds;
-$([IPython.events]).on("notebook_loaded.Notebook", function () {
-    IPython.notebook.minimum_autosave_interval = 30000;
-});
-
-// autoscroll is my greatest shame:
-IPython.OutputArea.auto_scroll_threshold = 0;
-
-
-// when doing presentations, I hode the header and toolbar
-$([IPython.events]).on("app_initialized.NotebookApp", function () {
-    // hide cell toolbar select, I never use it
-    $('#ctb_select').hide().prev().hide();
-    // $('div#header').hide();
-    // $('div#maintoolbar').hide();
-});
-
-if (IPython.CodeCell) {
-      IPython.CodeCell.options_default.cm_config.lineWrapping = true;
-}
+require([
+    "jquery",
+    "base/js/namespace",
+    "base/js/events",
+    ], function ($, IPython) {
+        
+        var load = function () {
+            if (!IPython.events) {
+                // interim fix: wait for events to exist
+                setTimeout(load, 100);
+                return;
+            }
+            console.log('...load');
+            var events = $([IPython.events]);
+            IPython.load_extensions("gist", "toc", "celltags");
+            events.on("app_initialized.NotebookApp", function () {
+                IPython.CodeCell.options_default.cm_config.lineWrapping = true;
+                // autoscroll is my greatest shame:
+                IPython.OutputArea.auto_scroll_threshold = 0;
+            
+                // hide cell toolbar select, I never use it
+                // $('#ctb_select').hide().prev().hide();
+            
+                // when doing presentations, I hide the header and toolbar
+                // $('div#header').hide();
+                // $('div#maintoolbar').hide();
+            });
+        
+            events.on("notebook_loaded.Notebook", function () {
+                // load the gist and toc extensions
+                // autosave every 30 seconds;
+                IPython.notebook.minimum_autosave_interval = 30000;
+            });
+        }
+        load();
+    }
+);
