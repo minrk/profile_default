a232c60fc3d5900c258dd927eb2693cc961db4cc
MinRK <benjaminrk@gmail.com>
2014-08-13 21:09:26 -0700

    custom.js update with ^2/^3 shortcuts

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -1,37 +1,35 @@
-require([
-    "jquery",
-    "base/js/namespace",
-    "base/js/events",
-    ], function ($, IPython) {
-        
-        var load = function () {
-            if (!IPython.events) {
-                // interim fix: wait for events to exist
-                setTimeout(load, 100);
-                return;
-            }
-            var events = $([IPython.events]);
-            // load the gist and toc extensions
-            IPython.load_extensions("gist", "toc", "celltags");
-            
-            events.on("app_initialized.NotebookApp", function () {
-                IPython.CodeCell.options_default.cm_config.lineWrapping = true;
-                // autoscroll is my greatest shame:
-                IPython.OutputArea.auto_scroll_threshold = 0;
-            
-                // hide cell toolbar select, I never use it
-                $('#ctb_select').hide().prev().hide();
-            
-                // when doing presentations, I hide the header and toolbar
-                // $('div#header').hide();
-                // $('div#maintoolbar').hide();
-            });
-        
-            events.on("notebook_loaded.Notebook", function () {
-                // autosave every 30 seconds;
-                IPython.notebook.minimum_autosave_interval = 30000;
-            });
-        }
-        load();
-    }
-);
+console.log("ok?");
+// load the gist and toc extensions
+IPython.load_extensions("gist", "toc");
+
+// autosave every 30 seconds;
+$([IPython.events]).on("notebook_loaded.Notebook", function () {
+    console.log("notebook loaded");
+    IPython.notebook.minimum_autosave_interval = 30000;
+});
+
+// when doing presentations, I hode the header and toolbar
+$([IPython.events]).on("app_initialized.NotebookApp", function () {
+    // hide cell toolbar select, I never use it
+    console.log('initialized');
+    $('#ctb_select').hide().prev().hide();
+    // $('div#header').hide();
+    // $('div#maintoolbar').hide();
+    IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
+
+    // autoscroll is my greatest shame:
+    IPython.OutputArea.auto_scroll_threshold = 0;
+
+    // ctrl-2/3 to toggle Python 2/3
+    IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-2', function (event) {
+        $("#kernel-python2").find("a").click();
+        return false;
+    });
+    IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-3', function (event) {
+        $("#kernel-python3").find("a").click();
+        return false;
+    });
+});
+
+// IPython.security.sanitize_html = function (html) { return html; };
+console.log("ok.");
