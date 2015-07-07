c0a36b45febfada035a4e29f90951ee1bf83e3ea
Min RK <benjaminrk@gmail.com>
2015-07-06 21:06:05 -0700

    update requirements

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -3,17 +3,18 @@ require(['notebook/js/cell', 'codemirror/keymap/sublime'], function (cell) {
     cell.Cell.options_default.cm_config.keyMap = 'sublime';
 });
 
-require(['base/js/namespace'], function () {
-    
+require(['jquery', 'base/js/namespace'], function ($) {
+
 $([IPython.events]).on("notebook_loaded.Notebook", function () {
     IPython.notebook.default_cell_type = 'above';
     IPython.notebook.minimum_autosave_interval = 30000;
 });
 
-// when doing presentations, I hode the header and toolbar
 $([IPython.events]).on("app_initialized.NotebookApp", function () {
     // load the gist and toc extensions
-    IPython.load_extensions("gist", "toc");
+    if (IPython.load_extensions) {
+        IPython.load_extensions("gist", "toc");
+    }
     
     // I have special CSS that only applies to the notebook
     $('body').addClass('notebook-body');
@@ -21,6 +22,7 @@ $([IPython.events]).on("app_initialized.NotebookApp", function () {
     // hide cell toolbar and cell type select, I never use it
     $('#cell_type').hide();
     $('#ctb_select').hide().prev().hide();
+    // when doing presentations, I hide the header and toolbar
     // $('div#header').hide();
     // $('div#maintoolbar').hide();
     IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
